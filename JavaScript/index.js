04afc3aad06972269f2305bbb091637a2ca92cdf
iceticshacker7 <iceandcold18@gmail.com>
2023-09-04 16:56:08 +0530

    Javascript practice 1:

---
+++ JavaScript/index.js
@@ -0,0 +1,29 @@
+console.log("node js");
+let a = 5;
+console.log(a);
+if(a==5)console.log("yeah boy!!!");
+const p =12;
+// p =19; error assigning a constant
+let str = "kame hame ha";
+// a = "babbar";
+console.log(a);
+t = 999;
+console.log(t);
+let name = ["my","name","is","divyansh"];
+console.log(name);
+name.forEach(element => {
+    console.log(element)
+});
+let person = {name :"divyansh",age:21,bool:true};
+
+console.log(person.name ,person.age ,person.bool);
+console.log(t%a);
+a++;
+console.log(--a);
+console.log(40**3,21*77);
+console.log(2<=24);
+let po = (15>13)?3:"34";
+console.log(po);
+for(i=0;i<str.length;i++){
+    console.log(str.charAt(i) , str[i]);
+}
